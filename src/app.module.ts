cef0f44bba06d7c0e2d15aa2819c0a13916cdc14
Jorge Guacarán <guacaranjorge94@gmail.com>
2021-07-05 21:19:11 -0400

    Added users CRUD.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/timeline-wall')
+    MongooseModule.forRoot('mongodb://localhost:27017/timeline-wall'),
+    UsersModule,
+    // MongooseModule.forFeature([
+    //   {
+    //     name: 'User',
+    //     schema: UserSchema
+    //   }
+    // ]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService
+  ],
 })
 export class AppModule {}
